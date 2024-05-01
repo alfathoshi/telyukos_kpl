aa6479437a8c2870e07102e09fd47c595827ff83
wildanniam <wildanniam4@gmail.com>
2024-05-01 23:13:21 +0700

    add menu library: Login, main menu and pemilik kos

---
+++ telyukos-library/Menu/MainMenu/PilihRole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telyukos_library.Menu.MainMenu
+{
+    public class PilihRole
+    {
+        public PilihRole()
+        {
+            Console.WriteLine("===============================================");
+            Console.WriteLine("=             Aplikasi TelyuKos               =");
+            Console.WriteLine("=         Created by Kelompok 3 KPL           =");
+            Console.WriteLine("=      Wildan, Bintang, Arga, Farah, Faris    =");
+            Console.WriteLine("=    Tugas Besar Konstruksi Perangkat Lunak   =");
+            Console.WriteLine("===============================================");
+            Console.WriteLine("=                    ***                      =");
+            Console.WriteLine("=      Selamat Datang di Aplikasi TelyuKos    =");
+            Console.WriteLine("=             Tentukan Role Anda              =");
+            Console.WriteLine("=                    ***                      =");
+            Console.WriteLine("=                                             =");
+            Console.WriteLine("= 1. Pemilik Kos                              =");
+            Console.WriteLine("= 2. Penyewa Kos                              =");
+            Console.WriteLine("= 3. Kembali                                  =");
+            Console.WriteLine("===============================================");
+        }
+        
+    }
+}
