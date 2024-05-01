0c125c306dc6df5f7240a665cc8ac41d01864b9b
ArgaAAL <argalumunon9@gmail.com>
2024-05-01 19:44:15 +0700

    menambahkan fungsi login register, API authentication, tambahan program.cs

--- telyukos/Program.cs
+++ telyukos/Program.cs
@@ -1,8 +1,14 @@
-﻿using System.Net.Http.Json;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using telyukos;
 
 internal class Program
 {
+    private static User loggedInUser;
+    private static bool isLoggedIn = false;
+
     static async Task Main(string[] args)
     {
         HttpClient httpClient = new HttpClient();
@@ -11,99 +17,151 @@ internal class Program
         bool exit = false;
         while (!exit)
         {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Tambah Kos");
-            Console.WriteLine("2. Update Kos");
-            Console.WriteLine("3. Hapus Kos");
-            Console.WriteLine("4. Tampilkan Semua Kos");
-            Console.WriteLine("0. Keluar");
-            Console.Write("Pilihan Anda: ");
-            string choice = Console.ReadLine();
-
-            switch (choice)
+            if (!isLoggedIn)
             {
-                case "1":
-                    // Tambah Kos
-                    Console.Write("Nama Kos: ");
-                    string namaKos = Console.ReadLine();
-                    Console.Write("Harga Kos: ");
-                    int hargaKos = int.Parse(Console.ReadLine());
-                    Console.Write("Alamat Kos: ");
-                    string alamatKos = Console.ReadLine();
+                Console.WriteLine("Login or Register:");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Register");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choice: ");
+                string choice = Console.ReadLine();
 
-                    Kos newKos = new Kos { Nama = namaKos, Harga = hargaKos, Alamat = alamatKos };
-                    HttpResponseMessage responsePost = await httpClient.PostAsJsonAsync("api/Kos", newKos);
-                    responsePost.EnsureSuccessStatusCode();
-                    string responseBodyPost = await responsePost.Content.ReadAsStringAsync();
-                    Console.WriteLine("Response POST:");
-                    Console.WriteLine(responseBodyPost);
-                    break;
+                switch (choice)
+                {
+                    case "1":
+                        // Login
+                        Console.Write("Email: ");
+                        string email = Console.ReadLine();
+                        Console.Write("Password: ");
+                        string password = Console.ReadLine();
 
-                case "2":
-                    // Update Kos
-                    Console.Write("Index Kos yang akan diupdate: ");
-                    int indexUpdate = int.Parse(Console.ReadLine());
-                    Console.Write("Nama Kos baru: ");
-                    string namaKosUpdate = Console.ReadLine();
-                    Console.Write("Harga Kos baru: ");
-                    int hargaKosUpdate = int.Parse(Console.ReadLine());
-                    Console.Write("Alamat Kos baru: ");
-                    string alamatKosUpdate = Console.ReadLine();
+                        User loginUser = new User { Email = email, Password = password };
+                        HttpResponseMessage responseLogin = await httpClient.PostAsJsonAsync("api/Auth/login", loginUser);
+                        responseLogin.EnsureSuccessStatusCode();
+                        string responseBodyLogin = await responseLogin.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response Login:");
+                        Console.WriteLine(responseBodyLogin);
 
-                    Kos updatedKos = new Kos { Nama = namaKosUpdate, Harga = hargaKosUpdate, Alamat = alamatKosUpdate };
-                    HttpResponseMessage responsePut = await httpClient.PutAsJsonAsync($"api/Kos/{indexUpdate}", updatedKos);
-                    responsePut.EnsureSuccessStatusCode();
-                    string responseBodyPut = await responsePut.Content.ReadAsStringAsync();
-                    Console.WriteLine("Response PUT:");
-                    Console.WriteLine(responseBodyPut);
-                    break;
+                        if (responseBodyLogin == "Login successful")
+                        {
+                            isLoggedIn = true;
+                        }
+                        break;
 
-                case "3":
-                    // Hapus Kos
-                    Console.Write("Index Kos yang akan dihapus: ");
-                    int indexDelete = int.Parse(Console.ReadLine());
+                    case "2":
+                        // Register
+                        Console.Write("Email: ");
+                        string newEmail = Console.ReadLine();
+                        Console.Write("Password: ");
+                        string newPassword = Console.ReadLine();
 
-                    HttpResponseMessage responseDelete = await httpClient.DeleteAsync($"api/Kos/{indexDelete}");
-                    responseDelete.EnsureSuccessStatusCode();
-                    string responseBodyDelete = await responseDelete.Content.ReadAsStringAsync();
-                    Console.WriteLine("Response DELETE:");
-                    Console.WriteLine(responseBodyDelete);
-                    break;
+                        User newUser = new User { Email = newEmail, Password = newPassword };
+                        HttpResponseMessage responseRegister = await httpClient.PostAsJsonAsync("api/Auth/register", newUser);
+                        responseRegister.EnsureSuccessStatusCode();
+                        string responseBodyRegister = await responseRegister.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response Register:");
+                        Console.WriteLine(responseBodyRegister);
+                        break;
 
-                case "4":
-                    // Tampilkan Semua Kos
-                    HttpResponseMessage responseGetAll = await httpClient.GetAsync("api/Kos");
-                    responseGetAll.EnsureSuccessStatusCode();
-                    Kos[] allKos = await responseGetAll.Content.ReadFromJsonAsync<Kos[]>();
+                    case "0":
+                        Environment.Exit(0);
+                        break;
 
-                    // Urutkan data berdasarkan ID
-                    allKos = allKos.OrderBy(k => k.Id).ToArray();
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Add Kos");
+                Console.WriteLine("2. Update Kos");
+                Console.WriteLine("3. Delete Kos");
+                Console.WriteLine("4. Show All Kos");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choice: ");
+                string menuChoice = Console.ReadLine();
 
-                    Console.WriteLine("Data Kos:");
-                    foreach (var kos in allKos)
-                    {
-                        Console.WriteLine($"ID: {kos.Id}, Nama: {kos.Nama}, Harga: {kos.Harga}, Alamat: {kos.Alamat}");
-                    }
-                    break;
+                switch (menuChoice)
+                {
+                    case "1":
+                        // Tambah Kos
+                        Console.Write("Nama Kos: ");
+                        string namaKos = Console.ReadLine();
+                        Console.Write("Harga Kos: ");
+                        int hargaKos = int.Parse(Console.ReadLine());
+                        Console.Write("Alamat Kos: ");
+                        string alamatKos = Console.ReadLine();
 
-                case "0":
-                    exit = true;
-                    break;
+                        Kos newKos = new Kos { Nama = namaKos, Harga = hargaKos, Alamat = alamatKos };
+                        HttpResponseMessage responsePost = await httpClient.PostAsJsonAsync("api/Kos", newKos);
+                        responsePost.EnsureSuccessStatusCode();
+                        string responseBodyPost = await responsePost.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response POST:");
+                        Console.WriteLine(responseBodyPost);
+                        break;
 
-                default:
-                    Console.WriteLine("Pilihan tidak valid. Silakan pilih lagi.");
-                    break;
-    }
+                    case "2":
+                        // Update Kos
+                        Console.Write("Index Kos yang akan diupdate: ");
+                        int indexUpdate = int.Parse(Console.ReadLine());
+                        Console.Write("Nama Kos baru: ");
+                        string namaKosUpdate = Console.ReadLine();
+                        Console.Write("Harga Kos baru: ");
+                        int hargaKosUpdate = int.Parse(Console.ReadLine());
+                        Console.Write("Alamat Kos baru: ");
+                        string alamatKosUpdate = Console.ReadLine();
+
+                        Kos updatedKos = new Kos { Nama = namaKosUpdate, Harga = hargaKosUpdate, Alamat = alamatKosUpdate };
+                        HttpResponseMessage responsePut = await httpClient.PutAsJsonAsync($"api/Kos/{indexUpdate}", updatedKos);
+                        responsePut.EnsureSuccessStatusCode();
+                        string responseBodyPut = await responsePut.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response PUT:");
+                        Console.WriteLine(responseBodyPut);
+                        break;
+
+                    case "3":
+                        // Hapus Kos
+                        Console.Write("Index Kos yang akan dihapus: ");
+                        int indexDelete = int.Parse(Console.ReadLine());
+
+                        HttpResponseMessage responseDelete = await httpClient.DeleteAsync($"api/Kos/{indexDelete}");
+                        responseDelete.EnsureSuccessStatusCode();
+                        string responseBodyDelete = await responseDelete.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response DELETE:");
+                        Console.WriteLine(responseBodyDelete);
+                        break;
+
+                    case "4":
+                        // Tampilkan Semua Kos
+                        HttpResponseMessage responseGetAll = await httpClient.GetAsync("api/Kos");
+                        responseGetAll.EnsureSuccessStatusCode();
+                        Kos[] allKos = await responseGetAll.Content.ReadFromJsonAsync<Kos[]>();
+
+                        // Urutkan data berdasarkan ID
+                        allKos = allKos.OrderBy(k => k.Id).ToArray();
+
+                        Console.WriteLine("Data Kos:");
+                        foreach (var kos in allKos)
+                        {
+                            Console.WriteLine($"ID: {kos.Id}, Nama: {kos.Nama}, Harga: {kos.Harga}, Alamat: {kos.Alamat}");
+                        }
+                        break;
+
+                    case "0":
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Pilihan tidak sesuai.");
+                        break;
+                }
+            }
 
             Console.WriteLine();
         }
     }
 
 }
-
-/*CRUD Pemilik Kos {
-	Create Kos (Automata, Table Driven, RunCog, API)
-	Read Kos (Library, API)
-	Edit Kos (Automata, Table Driven, RunCog, API)
-	Delete Kos (Automata, Table Driven, RunCog, API)
-}*/
