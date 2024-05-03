44bb7a52b5848e39d50116a063e04caadba43a95
faraamalia <frarsa18@gmail.com>
2024-05-04 03:49:17 +0700

    Table Driven Status Kamar Kos Sewa

--- telyukos/Program.cs
+++ telyukos/Program.cs
@@ -1,91 +1,194 @@
-﻿using Newtonsoft.Json;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Reflection.Emit;
+using telyukos_library.Menu;
+using telyukos;
+using static telyukos.Akun;
+using telyukos_library.Menu.MainMenu;
 using telyukos_library.Searching;
-using telyukos_library.Common;
-using System.Reflection.Emit;
+using PenyewaKos;
+
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        // ========== Tutorial make binary search nya ==========
+        Akun a = new Akun();
+      
+        a.ActiveTrigger(AkunTrigger.TULIS_START);
+        MainMenu mm = new MainMenu();
+        string Pilih = Console.ReadLine();
 
-        //int[] intArray = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-        //double[] doubleArray = { 1.2, 3.4, 5.6, 7.8, 9.0, 10.1, 12.3, 14.5, 16.7, 18.9 };
-        //string[] stringArray = { "apple", "banana", "cherry", "date", "grape", "kiwi", "mango" };
-
-        //Console.WriteLine("Searching in intArray:");
-        //Console.WriteLine("Index of 10: " + BinarySearch<int>.Search(intArray, 10));
-        //Console.WriteLine("Index of 13: " + BinarySearch<int>.Search(intArray, 13));
-
-        //Console.WriteLine("\nSearching in doubleArray:");
-        //Console.WriteLine("Index of 10.1: " + BinarySearch<double>.Search(doubleArray, 10.1));
-        //Console.WriteLine("Index of 11.0: " + BinarySearch<double>.Search(doubleArray, 11.0));
-
-        //Console.WriteLine("\nSearching in stringArray:");
-        //Console.WriteLine("Index of 'banana': " + BinarySearch<string>.Search(stringArray, "banana"));
-        //Console.WriteLine("Index of 'lemon': " + BinarySearch<string>.Search(stringArray, "lemon"));
-
-        // ========== Tutorial make Sequential Search nya ==========
-
-        //int[] intArray = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-        //double[] doubleArray = { 1.2, 3.4, 5.6, 7.8, 9.0, 10.1, 12.3, 14.5, 16.7, 18.9 };
-        //string[] stringArray = { "apple", "banana", "cherry", "date", "grape", "kiwi", "mango" };
-
-        //Console.WriteLine("Searching in intArray:");
-        //Console.WriteLine("Index of 10: " + SequentialSearch<int>.Search(intArray, 10));
-        //Console.WriteLine("Index of 13: " + SequentialSearch<int>.Search(intArray, 13));
-
-        //Console.WriteLine("\nSearching in doubleArray:");
-        //Console.WriteLine("Index of 10.1: " + SequentialSearch<double>.Search(doubleArray, 10.1));
-        //Console.WriteLine("Index of 11.0: " + SequentialSearch<double>.Search(doubleArray, 11.0));
-
-        //Console.WriteLine("\nSearching in stringArray:");
-        //Console.WriteLine("Index of 'banana': " + SequentialSearch<string>.Search(stringArray, "banana"));
-        //Console.WriteLine("Index of 'lemon': " + SequentialSearch<string>.Search(stringArray, "lemon"));
-
-
-        // ========== Tutorial make generate ID
-        int numberOfIDs = 20; // Jumlah ID yang ingin dihasilkan
-
-        Console.WriteLine("Generated IDs:");
-
-        for (int i = 0; i < numberOfIDs; i++)
+        while (Pilih != "0")
         {
-            string id = RandomIntegerGenerator.GenerateID();
-            Console.WriteLine(id);
+            if (Pilih == "1")
+            {
+                a.ActiveTrigger(AkunTrigger.PILIH_LOGIN);
+                Console.WriteLine("###     -------- Login --------     ###");
+                Console.WriteLine();
+                Console.WriteLine("Login as : ");
+                Console.WriteLine("1. Pemilik Kos");
+                Console.WriteLine("2. Penyewa Kos");
+                Console.WriteLine("0. Keluar");
+                Console.Write("Pilih: ");
+                string Pilih1 = Console.ReadLine();
+                if (Pilih1 == "1")
+                {
+                    a.ActiveTrigger(AkunTrigger.PILIH_PEMILIK);
+                    Console.WriteLine("Isi Data Login");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    Console.WriteLine();
+
+                    a.ActiveTrigger(AkunTrigger.MENGISI_DATA);
+                    Console.WriteLine("Selesai Isi Data :)");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Login");
+                    Console.WriteLine("2. Batal");
+                    Console.Write("Pilih: ");
+                    string Pilih2 = Console.ReadLine();
+                    if (Pilih2 == "1")
+                    {
+                        a.ActiveTrigger(AkunTrigger.LOGIN_DITERIMA);
+                        
+                        //int idx = SequentialSearch<string>.Search(usn, usernameLogin);
+                        //int idx1 = SequentialSearch<string>.Search(pw, passwordLogin);
+                        /*if (idx != -1 && idx1 != -1)
+                        {
+                            a.ActiveTrigger(AkunTrigger.LOGIN_DITERIMA);
+                        } 
+                        else
+                        {
+                            a.ActiveTrigger(AkunTrigger.LOGIN_DITOLAK);
+                        }*/
+                    }
+                    else if (Pilih2 == "2")
+                    {
+                        a.ActiveTrigger(AkunTrigger.BATAL);
+                        Console.WriteLine("Batal Login...");
+                    }
+                }
+                else if (Pilih1 == "2")
+                {
+                    a.ActiveTrigger(AkunTrigger.PILIH_PENYEWA);
+                    Console.WriteLine("Isi Data Login");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    Console.WriteLine();
+
+                    a.ActiveTrigger(AkunTrigger.MENGISI_DATA);
+                    Console.WriteLine("Selesai Isi Data :)");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Login");
+                    Console.WriteLine("2. Batal");
+                    Console.Write("Pilih: ");
+                    string Pilih2 = Console.ReadLine();
+                    if (Pilih2 == "1")
+                    {
+                        //int idx = SequentialSearch<string>.Search(usn, usernameLogin);
+                        //int idx1 = SequentialSearch<string>.Search(pw, passwordLogin);
+                        /*if (idx != -1 && idx1 != -1)
+                        {
+                            a.ActiveTrigger(AkunTrigger.LOGIN_DITERIMA);
+                        } 
+                        else
+                        {
+                            a.ActiveTrigger(AkunTrigger.LOGIN_DITOLAK);
+                        }*/
+                    }
+                    else if (Pilih2 == "2")
+                    {
+                        a.ActiveTrigger(AkunTrigger.BATAL);
+                        Console.WriteLine("Batal Login...");
+                    }
+                }
+                else if (Pilih1 == "0")
+                {
+                    a.ActiveTrigger(AkunTrigger.BATAL); Console.WriteLine("Batal Login..."); //Masih error
+                }
+            }
+            else if (Pilih == "2")
+            {
+                a.ActiveTrigger(AkunTrigger.PILIH_REGIS);
+                Console.WriteLine("###     -------- Registrasi --------     ###");
+                Console.WriteLine();
+                Console.WriteLine("Regist as : ");
+                Console.WriteLine("1. Pemilik Kos");
+                Console.WriteLine("2. Penyewa Kos");
+                Console.WriteLine("0. Keluar");
+                Console.Write("Pilih: ");
+                string Pilih3 = Console.ReadLine();
+
+                if (Pilih3 == "1")
+                {
+                    a.ActiveTrigger(AkunTrigger.PILIH_PEMILIK);
+                    Console.WriteLine("Isi Data Registrasi");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    Console.WriteLine();
+
+                    a.ActiveTrigger(AkunTrigger.MENGISI_DATA);
+                    Console.WriteLine("Selesai Isi Data :)");
+                    Console.WriteLine();
+
+                    Console.WriteLine("1. Buat Akun");
+                    Console.WriteLine("2. Batal");
+                    Console.Write("Pilih: ");
+                    string Pilih4 = Console.ReadLine();
+                    if (Pilih4 == "1")
+                    {
+                        a.ActiveTrigger(AkunTrigger.REGISTER_BERHASIL);
+                    }
+                    else if (Pilih4 == "2")
+                    {
+                        a.ActiveTrigger(AkunTrigger.BATAL);
+                    }
+
+                }
+                else if (Pilih3 == "2")
+                {
+                    a.ActiveTrigger(AkunTrigger.PILIH_PENYEWA);
+                    Console.WriteLine("Isi Data Registrasi");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    Console.WriteLine();
+                    a.ActiveTrigger(AkunTrigger.MENGISI_DATA);
+                    Console.WriteLine("Selesai Isi Data :)");
+                    Console.WriteLine();
+
+                    Console.WriteLine("1. Buat Akun");
+                    Console.WriteLine("2. Batal");
+                    Console.Write("Pilih: ");
+                    string Pilih4 = Console.ReadLine();
+                    if (Pilih4 == "1")
+                    {
+                        a.ActiveTrigger(AkunTrigger.REGISTER_BERHASIL);
+                    }
+                    else if (Pilih4 == "2")
+                    {
+                        a.ActiveTrigger(AkunTrigger.BATAL);
+                    }
+                }
+                else if (Pilih3 == "0")
+                {
+                    a.ActiveTrigger(AkunTrigger.BATAL); Console.WriteLine("Batal Registrasi..."); //Masih error
+                }
+
+            }
+            else if (Pilih == "0")
+            {
+                a.ActiveTrigger(AkunTrigger.BATAL);  //Masih error
+            }
         }
 
-        // ========== tutorial parsing pake library newtonsoft ==========
-        //Mahasiswa objMhs = new Mahasiswa();
-
-        //objMhs.NIM = 122011389;
-        //objMhs.nama = "Alan";
-        //objMhs.alamat = "Bandung";
-
-        //// Serialisasi
-        //string JsonOutpuMahasiswa = JsonConvert.SerializeObject(objMhs);
-        //// pastiin letak folder nya sesuai, yg dibawah cmn contoh ya
-        //File.WriteAllText("D://JSON/Mahasiswa.json", JsonOutpuMahasiswa);
-
-        // Deserialisasi
-        //string HasilBacaJson = File.ReadAllText("D://JSON/Mahasiswa.json");
-        //// pastiin letak folder nya sesuai, yg dibawah cmn contoh ya
-        //Mahasiswa MhsBaca = JsonConvert.DeserializeObject<Mahasiswa>(HasilBacaJson);
-
-        //Console.WriteLine("======== Newtonsoft.Json ========");
-        //Console.WriteLine("NIM: " + MhsBaca.NIM);
-        //Console.WriteLine("Nama: " + MhsBaca.nama);
-        //Console.WriteLine("Alamat: " + MhsBaca.alamat);
     }
-
-    // buat class yang mau diparsing duluu
-    public class Mahasiswa
-    {
-        public int NIM;
-        public string nama;
-        public string alamat;
-    }
-
 }
+
+
+
