47b275e79bd6754d5117611d79e0df60032bf408
Wildan Syukri Niam <120638975+wildanniam@users.noreply.github.com>
2024-05-05 00:15:19 +0700

    pindah branch

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34eb63cee69c3ffcfc4379dab621fceb09fe734")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
