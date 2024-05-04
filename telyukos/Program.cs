2a8e37316906c8c49f36e9449b9d98f78cfcf18b
fara <153857497+faraamalia@users.noreply.github.com>
2024-05-05 00:02:56 +0700

    Update Program.cs

--- telyukos/Program.cs
+++ telyukos/Program.cs
@@ -1,12 +1,11 @@
-using PenyewaKos;
 using System;
-using static PenyewaKos.ReservasiKos;
+using telyukos;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ReservasiKos reservasi = new ReservasiKos(); // Membuat objek ReservasiKos
+        ReservasiKost reservasi = new ReservasiKost(); // Membuat objek ReservasiKos
 
         while (true) // Loop utama program
         {
@@ -14,18 +13,18 @@ class Program
 
             switch (reservasi.currentState) // Menentukan aksi berdasarkan state saat ini
             {
-                case ReservasiState.START:
+                case ReservasiKost.ReservasiState.START:
                     Console.WriteLine("===== RESERVASI KOS =====");
                     Console.WriteLine("Tekan Enter untuk memulai...");
                     Console.ReadLine();
-                    reservasi.ActiveTrigger(ReservasiTrigger.MULAI);
+                    reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.MULAI);
                     break;
 
-                case ReservasiState.LIHAT_INFORMASI_KOS:
-                    reservasi.ActiveTrigger(ReservasiTrigger.MELIHAT_KOS);
+                case ReservasiKost.ReservasiState.LIHAT_INFORMASI_KOS:
+                    reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.MELIHAT_KOS);
                     break;
 
-                case ReservasiState.PILIH_KOS:
+                case ReservasiKost.ReservasiState.PILIH_KOS:
                     Console.Write("Pilih Kos: ");
                     string chooseKos = Console.ReadLine();
 
@@ -33,52 +32,52 @@ class Program
                     {
                         case "1":
                             Console.WriteLine("Anda memilih Kos Mawar");
-                            reservasi.ActiveTrigger(ReservasiTrigger.PILIH_RESERVASI);
+                            reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.PILIH_RESERVASI);
                             break;
                         case "2":
                             Console.WriteLine("Anda memilih Kos Melati");
-                            reservasi.ActiveTrigger(ReservasiTrigger.PILIH_RESERVASI);
+                            reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.PILIH_RESERVASI);
                             break;
                         case "3":
                             Console.WriteLine("Anda memilih Kos Semuanya Indah");
-                            reservasi.ActiveTrigger(ReservasiTrigger.PILIH_RESERVASI);
+                            reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.PILIH_RESERVASI);
                             break;
                         default:
                             Console.WriteLine("Pilihan tidak valid.");
-                            reservasi.ActiveTrigger(ReservasiTrigger.BACK);
+                            reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.BACK);
                             Console.ReadLine();
                             break;
                     }
                     break;
 
-                case ReservasiState.RESERVASI:
-                    reservasi.ActiveTrigger(ReservasiTrigger.MENGISI_DATA);
+                case ReservasiKost.ReservasiState.RESERVASI:
+                    reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.MENGISI_DATA);
                     break;
 
-                case ReservasiState.DAFTAR_RESERVASI:
-                    // Simulasi proses approval/penolakan reservasi penyewa 
+                case ReservasiKost.ReservasiState.DAFTAR_RESERVASI:
+                    // Simulasi proses approval/penolakan reservasi penyewaÂ 
                     int randomApproval = new Random().Next(0, 2); // 0: Ditolak, 1: Diterima
 
                     if (randomApproval == 0)
                     {
-                        reservasi.ActiveTrigger(ReservasiTrigger.DAFTAR_DITOLAK);
+                        reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.DAFTAR_DITOLAK);
                     }
                     else
                     {
-                        reservasi.ActiveTrigger(ReservasiTrigger.DAFTAR_DITERIMA);
+                        reservasi.ActiveTrigger(ReservasiKost.ReservasiTrigger.DAFTAR_DITERIMA);
                     }
                     break;
 
-                case ReservasiState.RESERVASI_BERHASIL:
+                case ReservasiKost.ReservasiState.RESERVASI_BERHASIL:
                     Console.WriteLine("Tekan Enter untuk kembali ke awal...");
                     Console.ReadLine();
-                    reservasi.currentState = ReservasiState.START;
+                    reservasi.currentState = ReservasiKost.ReservasiState.START;
                     break;
 
-                case ReservasiState.RESERVASI_GAGAL:
+                case ReservasiKost.ReservasiState.RESERVASI_GAGAL:
                     Console.WriteLine("Tekan Enter untuk kembali ke awal...");
                     Console.ReadLine();
-                    reservasi.currentState = ReservasiState.START;
+                    reservasi.currentState = ReservasiKost.ReservasiState.START;
                     break;
             }
         }
